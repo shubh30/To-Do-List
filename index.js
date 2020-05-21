615cf94506626b738c399c0fa544240fd8c094d8
Shubham <shubhamnagpal30@gmail.com>
2020-05-21 22:10:02 +0530

    Added view and setup with it

--- index.js
+++ index.js
@@ -3,12 +3,32 @@ const port = 8000;
 
 const app = express();
 
+// Setting the Value for the property "View Engine" as "Ejs" (Setting Up View engine)
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+var toDoList = [
+    {
+        todo: "Eat Breakfast"
+    },
+    {
+        todo: "Watch Movie"
+    },
+    {
+        todo: "Try Not to Study"
+    }
+]
 
 app.get('/', function(req, res) {
-    res.send('<h1>It is running</h1>');
+    return res.render('home', {
+        title: "To Do List",
+        todo_list: toDoList
+    });
 });
 
-
+app.post('/create-todo', function(req, res) {
+    return res.redirect('back');
+});
 
 app.listen(port, function(err) {
     if(err) {
