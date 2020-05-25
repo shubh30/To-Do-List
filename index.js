59260d34897a50923fab5dc130c964cc5622af07
Shubham <shubhamnagpal30@gmail.com>
2020-05-25 22:38:19 +0530

    Set up server :: MongoDB

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const port = 8000;
-
+const db = require('./config/mogoose');
+const Todo = require('./models/todo');
 const app = express();
 
 // Setting the Value for the property "View Engine" as "Ejs" (Setting Up View engine)
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// Parser as middlware
 app.use(express.urlencoded());
 app.use(express.static('assets'))
 
@@ -22,16 +25,44 @@ var toDoList = [
 ]
 
 app.get('/', function(req, res) {
-    return res.render('home', {
-        title: "To Do List",
-        todo_list: toDoList
+
+    Todo.find({}, function(err, todos){
+        if(err){
+            console.log('Error in fetching contact from db');
+            return;
+        }
+        
+        return res.render('home', {
+            title: "To Do List",
+            todo_list: todos
+        });
     });
 });
 
 app.post('/create-todo', function(req, res) {
-    toDoList.push(req.body);
-    
-    return res.redirect('back');
+    // toDoList.push(req.body);
+    Todo.create({
+        task: req.body.name
+    }, function(err, newTodo){
+        if(err){console.log('Error in creating a todo'); return;}
+        console.log('*******', newTodo);
+        return res.redirect('back');
+    });
+});
+
+app.get('/delete-contact/', function(req, res){
+    // Get the id from the query in the url
+    let id = req.query.id;
+
+    // Find the todo in the database using id and delete
+    Todo.findByIdAndDelete(id, function(err){
+        if(err) {
+            console.log('Error in deleting an obj fron db');
+            return;
+        }
+
+        return res.redirect('back');
+    });
 });
 
 app.listen(port, function(err) {
